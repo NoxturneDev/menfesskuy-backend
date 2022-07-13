877b11d252f1f6e191b3bf6ee549964af7c2ca0c
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-07-13 15:56:19 +0700

    depecrating generate link function and API

--- controller/User-controller.js
+++ controller/User-controller.js
@@ -24,11 +24,15 @@ exports.registerUser = async (req, res) => {
 
     const salt = await bcrypt.genSalt()
     const hashPassword = await bcrypt.hash(password, salt)
+    // GENERATE UNIQUE LINK FOR EACH USER
+    const randomNum = Math.floor(Math.random() * 90 + 10)
+    const link = `${username}~${randomNum}!`
 
     try {
         const user = await Users.create({
             username: username,
-            password: hashPassword
+            password: hashPassword,
+            user_link: link
         })
         return res.status(200).json({ msg: 'Mantap', user })
     } catch (err) {
@@ -54,7 +58,8 @@ exports.Login = async (req, res) => {
         const userId = user[0].id
         const userName = user[0].username
         const user_link = user[0].user_link
-        console.log(userId, userName)
+
+
         const accessToken = jwt.sign({ userId, userName, user_link }, process.env.SECRET_ACCESS_TOKEN, {
             expiresIn: '15s'
         })
@@ -106,35 +111,32 @@ exports.Logout = async (req, res) => {
     }
 }
 
-exports.generateLink = async (req, res) => {
-    try {
-        // AUTHORIZATION WITH JWT
-        const refreshToken = req.cookies.refreshToken
-        if (!refreshToken) return res.sendStatus(401)
+// !!DEPRECATED
+// exports.generateLink = async (req, res) => {
+//     try {
+//         // AUTHORIZATION WITH JWT
+//         const refreshToken = req.cookies.refreshToken
+//         if (!refreshToken) return res.sendStatus(401)
 
-        const user = await Users.findAll({
-            where: {
-                refresh_token: refreshToken
-            }
-        })
+//         const user = await Users.findAll({
+//             where: {
+//                 refresh_token: refreshToken
+//             }
+//         })
 
-        // GENERATE UNIQUE STRING FOR THE LINK BASED ON USER INFORMATION
-        const username = user[0].username.split(' ')[0]
-        const randomNum = Math.floor(Math.random() * 90 + 10)
-        const userId = user[0].id
-        const link = `${username}${userId}~${randomNum}!`
+//         // GENERATE UNIQUE STRING FOR THE LINK BASED ON USER INFORMATION
 
-        // VALIDATE IF THERE'S ALREADY A LINK
-        if (user[0].user_link !== null) {
-            return res.status(401).json({ msg: 'You already have a link' })
-        }
-        await Users.update({ user_link: link }, {
-            where: {
-                id: userId
-            }
-        })
-        res.status(200).json({ msg: 'link generated', link })
-    } catch (err) {
-        console.error(err)
-    }
-}
+//         // VALIDATE IF THERE'S ALREADY A LINK
+//         if (user[0].user_link !== null) {
+//             return res.status(401).json({ msg: 'You already have a link' })
+//         }
+//         await Users.update({ user_link: link }, {
+//             where: {
+//                 id: userId
+//             }
+//         })
+//         res.status(200).json({ msg: 'link generated', link })
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
--- routes/Router.js
+++ routes/Router.js
@@ -1,4 +1,4 @@
-const { getUsers, registerUser, Login, Logout, generateLink } = require('../controller/User-controller.js')
+const { getUsers, registerUser, Login, Logout } = require('../controller/User-controller.js')
 const express = require('express')
 const { verifyToken, tokenRefresh } = require('../middleware/VerifyToken.js')
 const { getMessages, sendMessages } = require('../controller/Message-controller.js')
@@ -17,7 +17,7 @@ router.post('/api/send/message', sendMessages)
 //GET SPECIFIC USER BY GIVEN LINK
 router.post('/api/send/message/:link', sendMessages)
 router.get('/api/get/message/:link', verifyToken, getMessages)
-router.get('/api/get/link', verifyToken, generateLink)
+// router.get('/api/get/link', verifyToken, generateLink) DEPRECATED
 
 
 
