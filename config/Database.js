a35874064524987c3ad1d5381e9583e350093d70
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-07-19 22:38:13 +0700

    change configuration on db connection

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-    host: 'containers-us-west-29.railway.app',
-    dialect: 'mysql'
-})
+// const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+//     host: 'containers-us-west-29.railway.app',
+//     dialect: 'mysql'
+// })
+
+const db = new Sequelize('mysql://root:0thevizrOiBic7wi8XUe@containers-us-west-29.railway.app:6340/railway')
 
 
 module.exports = db
